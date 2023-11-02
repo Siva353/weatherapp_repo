872a84958646e8b4254e89eb4a2dce0a05daab43
siva123 <sivamani.nallabariki225@gmail.com>
2023-11-02 12:13:33 +0530

    first commit

---
+++ weather_app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask,request,render_template,jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/weatherapp', methods=['GET','POST'])
+def get_weather_data():
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    param = {
+        'q': request.form.get("city"),
+        'appid': request.form.get("appid"),
+        'units': request.form.get("units")
+        }
+    response = requests.get(url, params=param)
+    data = response.json()
+    return f"data : {data}"
+    
+if __name__ == '__main__':
+    app.run(debug=True)
